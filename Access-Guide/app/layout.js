b1fc8ff2a0efb305ec9c0ad852d8325adfa0c96b
itz-XD <152062675+itz-XD@users.noreply.github.com>
2024-08-02 13:28:59 +0530

    Access Guide

--- Access-Guide/app/layout.js
+++ Access-Guide/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='bg-white'>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
